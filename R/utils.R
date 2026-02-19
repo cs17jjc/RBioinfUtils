d102664e046dfa40b90676974df06312ab338df0
cs17jjc <33628960+cs17jjc@users.noreply.github.com>
2026-02-19 13:38:04 +0000

    Add gene symbols to rowData

--- R/utils.R
+++ R/utils.R
@@ -15,23 +15,41 @@ process_counts_table.character <- function(x, ...){
 
 #' @param x data.table containing featureCounts output
 #' @param col_to_sample_regex Regular expression with capture group to extract sample names from column names
-#' @param metadata_mapper Function created with metadata_mapper() that takes sample_name and returns list with Sample, Group, Site
+#' @param metadata_mapper Function that takes sample names and returns named list with extracted metadata
+#' @param orgdb Optional OrgDb object for gene annotation (e.g., org.Hs.eg.db, org.Mm.eg.db). If NULL, no symbols added.
 #' @return SummarizedExperiment object with counts assay and sample metadata
 #' @export
 process_counts_table.data.table <- function(x,
                                             col_to_sample_regex = ".*/(.*?)_Aligned.*$",
-                                            metadata_mapper){
+                                            metadata_mapper,
+                                            orgdb = NULL){
   expr_matrix <- as.matrix(x[, 7:ncol(x)])
-
   sample_column_names <- colnames(expr_matrix)
   sample_names <- sub(col_to_sample_regex, "\\1", sample_column_names)
   colnames(expr_matrix) <- sample_names
   rownames(expr_matrix) <- x$Geneid
+
   metadata <- purrr::map_dfr(sample_names, metadata_mapper)
   rownames(metadata) <- metadata$Sample
+
+  gene_info <- x[, 1:6]
+
+  if (!is.null(orgdb)) {
+    gene_info$Symbol <- AnnotationDbi::mapIds(
+      orgdb,
+      keys = x$Geneid,
+      column = "SYMBOL",
+      keytype = "ENSEMBL",
+      multiVals = "first"
+    )
+  }
+
+  rownames(gene_info) <- x$Geneid
+
   SummarizedExperiment::SummarizedExperiment(
     assays = list(counts = expr_matrix),
-    colData = metadata
+    colData = metadata,
+    rowData = gene_info
   )
 }
 
